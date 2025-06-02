349c7365aba68fce4b5721135eb29bdb4d551b2d
HagenK42 <666.thurisaz.666@gmail.com>
2025-06-02 21:41:29 +0200

    changed the db-structure

--- api/models.py
+++ api/models.py
@@ -3,18 +3,20 @@ from django.db import models
 # Create your models here.
 
 class Employee(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    date_of_birth = models.DateField()
-    email = models.EmailField(max_length=100)
-    phone_number = models.CharField(max_length=100)
-    salary = models.IntegerField()
-    department = models.ForeignKey('Department', on_delete=models.CASCADE)
-    date_of_joining = models.DateField()
+    employee_first_name = models.CharField(max_length=100)
+    employee_last_name = models.CharField(max_length=100)
+    employee_date_of_birth = models.DateField()
+    employee_email = models.EmailField(max_length=100)
+    employee_phone_number = models.CharField(max_length=100)
+    employee_department = models.ForeignKey('Department', on_delete=models.CASCADE)
+    employee_position = models.CharField(max_length=100, default='Employee')
+    employee_salary = models.IntegerField()
+    employee_date_of_joining = models.DateField()
+
 
 class Department(models.Model):
     department_name = models.CharField(max_length=100)
     department_manager = models.ForeignKey('Employee', on_delete=models.CASCADE, related_name='managed_departments', null=True, blank=True)
-    department_location = models.CharField(max_length=100)
     department_budget = models.IntegerField()
-    department_phone_number = models.CharField(max_length=100)
+    department_revenue = models.IntegerField(default=0)
+    department_employees = models.ManyToManyField(Employee, related_name='departments', blank=True)
--- api/serializers.py
+++ api/serializers.py
@@ -4,9 +4,9 @@ from .models import Employee
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
-        fields = ['first_name', 'last_name', 'date_of_birth', 'email', 'phone_number', 'salary', 'department', 'date_of_joining']
+        fields = ['employee_first_name', 'employee_last_name', 'employee_date_of_birth', 'employee_email', 'employee_phone_number', 'employee_salary', 'employee_department', 'employee_position', 'employee_date_of_joining']
 
 class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Employee.department.field.related_model  # Assuming department is a ForeignKey in Employee
-        fields = ['department_name', 'department_manager', 'department_location', 'department_budget', 'department_phone_number']
+        model = Employee.employee_department.field.related_model  # Assuming department is a ForeignKey in Employee
+        fields = ['department_name', 'department_manager', 'department_budget', 'department_revenue']
